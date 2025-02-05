d6da41ea7e7980b256fda7a81233d6c06e67efe6
Fai-chebby <chebetf034@gmail.com>
2025-02-05 10:12:30 +0300

    code challenge

--- components/script.js
+++ components/script.js
@@ -1,3 +1,5 @@
+const { loadPreset } = require("@babel/core/lib/config/files");
+
 let js = "amazing";
 if (js === " amazing") alert("javascript is fun!");
 10 + 10;
@@ -17,3 +19,16 @@ const present = 2025;
 const age1 = present - 1990;
 const age2 = present - 2020;
 console.log(age1, age2);
+
+const marksmass = 78;
+const marksheight = 1.69;
+
+const johnmass = 92;
+const johnheight = 1.95;
+
+marksBMI = marksmass / (marksheight * marksheight);
+console.log(marksBMI);
+johnBMI = johnmass / (johnheight * johnheight);
+console.log(johnBMI);
+const markHigherBMI = marksBMI > johnBMI;
+console.log(markHigherBMI);
