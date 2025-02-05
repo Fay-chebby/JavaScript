06eada72c4f561d793613fff848d09edac4a563a
Fai-chebby <chebetf034@gmail.com>
2025-02-05 05:28:55 +0300

    operators

--- components/script.js
+++ components/script.js
@@ -5,3 +5,15 @@ console.log(10 + 10);
 console.log("Faith");
 let firstName = "faith";
 console.log(firstName);
+
+let age = 20;
+age = 21;
+// const declare variable that cannot change
+const yearofbirth = 1990;
+var job = "Programmer";
+job = "Chief producer";
+
+const present = 2025;
+const age1 = present - 1990;
+const age2 = present - 2020;
+console.log(age1, age2);
